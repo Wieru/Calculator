4837d7fae4a108d275cee8d26b67539c2ff505d3
Szymon Wierucki <szymon.wierucki@gmail.com>
2023-11-02 19:49:41 +0100

    Add js logic and scrollbar

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const previousNumber = document.querySelector('.previousNumber p');
 
 const mathSign = document.querySelector('.mathSign');
 
-const numberButtons = document.querySelectorAll('.number');
+const numbersButtons = document.querySelectorAll('.number');
 
 const operatorsButtons = document.querySelectorAll('.operator');
 
@@ -12,7 +12,7 @@ const equalsButton = document.querySelector('.equals');
 
 const clearButton = document.querySelector('.clear');
 
-const calculatorHistory = document.querySelector('.calculatory-history');
+const calculatorHistory = document.querySelector('.history');
 
 const historyBtn = document.querySelector('.history-btn');
 
@@ -23,23 +23,82 @@ let result = '';
 
 
 function displayNumbers() {
+    if(this.textContent === '.' && currentNumber.innerHTML.includes('.')) return;
+    if(this.textContent === '.' && currentNumber.innerHTML === '') return currentNumber.innerHTML = '.0';
 
+    currentNumber.innerHTML += this.textContent;
 }
 
 function operate() {
+    if(currentNumber.innerHTML === '' && this.textContent === '-') {
+        currentNumber.innerHTML = '-';
+        return;
+    }
 
+    else if (currentNumber.innerHTML === '') {
+        return};
+
+    if(mathSign.innerHTML !== '') {
+        showResult();
+    }
+
+    previousNumber.innerHTML = currentNumber.innerHTML;
+    mathSign.innerHTML = this.textContent;
+    currentNumber.innerHTML = '';
 }
 
 function showResult() {
+    if(previousNumber.innerHTML === '' || currentNumber.innerHTML === '') return;
+
+    let a = Number(currentNumber.innerHTML);
+    let b = Number(previousNumber.innerHTML);
+    let operator = mathSign.innerHTML;
+
+    switch(operator) {
+        case '+':
+            result = a + b;
+            break;
+        case '-':
+            result = b - a;
+            break;
+        case 'x':
+            result = a * b;
+            break;
+        case ':':
+            result = b / a;
+            break;
+        case '2^':
+            result = b ** a;
+            break;
+    }
+
+    addToHistory();
+    historyBtn.classList.add('active');
+    currentNumber.innerHTML = result;
+    previousNumber.innerHTML = '';
+    mathSign.innerHTML = '';
 
 }
 
-function clearScreen() {
-
+function addToHistory() {
+    const newHistoryItem = document.createElement('li');
+    newHistoryItem.innerHTML = `${currentNumber.innerHTML} ${mathSign.innerHTML} ${previousNumber.innerHTML} = ${result}`
+    newHistoryItem.classList.add('history-item');
+    calculatorHistory.appendChild(newHistoryItem);
 }
 
 function clearHistory() {
+    calculatorHistory.textContent = ''
+    if(calculatorHistory.textContent === '') {
+        historyBtn.classList.remove('active');
+    }
+}
 
+function clearScreen() {
+    result.innerHTML = '';
+    currentNumber.innerHTML = '';
+    previousNumber.innerHTML = '';
+    mathSign.innerHTML = '';
 }
 
 
@@ -58,6 +117,6 @@ equalsButton.addEventListener('click', showResult);
 
 clearButton.addEventListener('click', clearScreen);
 
-numberButtons.forEach((button) => button.addEventListener('click', displayNumbers));
+numbersButtons.forEach((button) => button.addEventListener('click', displayNumbers));
 
 historyBtn.addEventListener('click', clearHistory);
